889f12e73bc73a0b2702aa91a7e72d2abb3b1ef5
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-11 11:13:47 +0900

    mandatory check

--- db_2/form_insert_fin.php
+++ db_2/form_insert_fin.php
@@ -1,11 +1,20 @@
 <?php
 
+$error_flg = false;
+
 // パラメータの一蘭
-$params = array('name', 'post', 'address', 'birthday_yy', 'birthday_mm', 'birthday_dd');
+$validate_params = array('name', 'post', 'address', 'birthday_yy', 'birthday_mm', 'birthday_dd');
 // データを取得する
-foreach($params as $p) {
+foreach($validate_params as $p) {
     $user_input_data[$p] = (string)@$_POST[$p];
+
+    // 必須チェック
+    if('' === $user_input_data[$p]){
+        $error_flg = true;
+    }
 }
 
 // 確認
 var_dump($user_input_data);
+// 必須チェック
+var_dump($error_flg);
