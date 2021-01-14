3bffdee73018baa766b8e4f74b5c5b1350bec670
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-14 23:50:14 +0900

    copy form Github

--- db_2/form_insert_fin.php
+++ db_2/form_insert_fin.php
@@ -1,17 +1,31 @@
 <?php
+
 /*
  * ユーザからのform情報の取得とDBへのINSERT
  */
 
-/* ユーザ入力情報の取得 */
-
-// バッファリング
+// ユーザ入力情報の取得
+// --------------------------------------
+// HTTP responseヘッダを出力する可能性があるので、バッファリングしておく
 ob_start();
 // セッションの開始
 session_start();
 
+// 共通関数のinclude
+require_once('common_function.php');
+
 // ユーザ入力情報を保持する配列を準備する
 $user_input_data = array();
+
+/*
+// データを取得する
+$user_input_data['name'] = (string)@$_POST['name'];
+$user_input_data['post'] = (string)@$_POST['post'];
+$user_input_data['address'] = (string)@$_POST['address'];
+$user_input_data['birthday_yy'] = (string)@$_POST['birthday_yy'];
+$user_input_data['birthday_mm'] = (string)@$_POST['birthday_mm'];
+$user_input_data['birthday_dd'] = (string)@$_POST['birthday_dd'];
+*/
 
 // 「パラメタの一覧」を把握
 $params = array('name', 'post', 'address', 'birthday_yy', 'birthday_mm', 'birthday_dd');
@@ -19,26 +33,26 @@ $params = array('name', 'post', 'address', 'birthday_yy', 'birthday_mm', 'birthd
 foreach($params as $p) {
     $user_input_data[$p] = (string)@$_POST[$p];
 }
+// 確認
+//var_dump($user_input_data);
 
-/* ユーザ入力のvalidate */
-
-// エラーフラグ
+// ユーザ入力のvalidate
+// --------------------------------------
+//
 $error_flg = false;
-// エラーの詳細を入れる配列
-$error_detail = array();
+$error_detail = array(); // エラー情報の詳細を入れるための配列を用意する
 
 // 必須チェックを実装
 $validate_params = array('name', 'post', 'address', 'birthday_yy', 'birthday_mm', 'birthday_dd');
 foreach($validate_params as $p) {
     // 空文字(未入力)なら
     if ('' === $user_input_data[$p]) {
+        // 「必須情報の未入力エラー」であることを配列に格納しておく
+        $error_detail["error_must_{$p}"] = true; // 例えば「名前が未入力」の場合は、key名は「error_must_name」となる
         // エラーフラグを立てる
         $error_flg = true;
-        // 必須入力のみ入力エラー
-        $error_detail["error_must_{$p}"] = true;
     }
 }
-
 // 型チェックを実装
 // 郵便番号
 /*
@@ -49,10 +63,10 @@ foreach($validate_params as $p) {
     \z: 行末
 */
 if (1 !== preg_match('/\A[0-9]{3}[- ]?[0-9]{4}\z/', $user_input_data['post'])) {
+    // 「郵便番号のフォーマットエラー」であることを配列に格納しておく
+    $error_detail["error_format_post"] = true;
     // エラーフラグを立てる
     $error_flg = true;
-    // 郵便番号のフォーマットエラー
-    $error_detail["error_format_post"] = true;
 }
 
 // 誕生日
@@ -60,27 +74,40 @@ if (1 !== preg_match('/\A[0-9]{3}[- ]?[0-9]{4}\z/', $user_input_data['post'])) {
 $int_params = array('birthday_yy', 'birthday_mm', 'birthday_dd');
 foreach($int_params as $p) {
     $user_input_data[$p] = intval($user_input_data[$p]);
+    //$user_input_data[$p] = (int)$user_input_data[$p]; // こちらの書き方でもよい
 }
-
 // PHPの標準関数を使って日付の妥当性をチェックする
 if (false === checkdate($user_input_data['birthday_mm'], $user_input_data['birthday_dd'], $user_input_data['birthday_yy'])) {
+    // 「誕生日のフォーマットエラー」であることを配列に格納しておく
+    $error_detail["error_format_birthday"] = true;
     // エラーフラグを立てる
     $error_flg = true;
-    // 誕生日のフォーマットエラー
-    $error_detail["error_format_birthday"] = true;
 }
 
-// エラーが出た時、入力ページに遷移する
-if(true === $error_flg){
+// 確認
+//var_dump($error_flg);
 
-    // エラー情報をセッションに入れて持ち回る
+// CSRFチェック
+if (false === is_csrf_token()) {
+    // 「CSRFトークンエラー」であることを配列に格納しておく
+    $error_detail["error_csrf"] = true;
+    // エラーフラグを立てる
+    $error_flg = true;
+}
+
+// エラーが出たら入力ページに遷移する
+if (true === $error_flg) {
+    // エラー情報をセッションに入れて持ちまわる
     $_SESSION['output_buffer'] = $error_detail;
-    // 入力値をセッションに入れて持ち回る
-    $_SESSION['output_buffer'] = $user_input_data;
-    
+
+    // 入力値をセッションに入れて持ちまわる
+    // XXX 「keyが重複しない」はずなので、加算演算子でOK
+    $_SESSION['output_buffer'] += $user_input_data;
+
+    // 入力ページに遷移する
     header('Location: ./form_insert.php');
     exit;
 }
 
-// ダミーのOK
+// ダミーのOK：後で削除する
 echo 'OK';
